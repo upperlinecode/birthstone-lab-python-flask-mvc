c91a2970b3737596fa0033e08afd5e193bbbc3fe
EC2 Default User <ec2-user@ip-172-31-23-71.us-east-2.compute.internal>
2019-06-25 16:30:26 +0000

    adds formopener

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect
 from app import app
-from app.models import model
+from app.models import model, formopener
 
 @app.route('/')
 @app.route('/index')
